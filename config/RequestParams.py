4efcc6dfcb82c46ae3d41ebbccf7a97e6c3990c6
LehiChiang <1471277588@qq.com>
2020-04-20 11:55:04 +0800

    优化代码；新增爬取全部文章

---
+++ config/RequestParams.py
@@ -0,0 +1,28 @@
+import json
+
+
+def get_request_params(biz, offset, count, key, appmsg_token='', pass_ticket=''):
+    # key,__biz和appmsg_token的属性是不一样的
+    return {
+            'action': 'getmsg',
+            '__biz': biz,
+            'f': 'json',
+            'offset': offset,  # offset是改变的，它的值是上次的offset的值加上上次的count值
+            'count': count,
+            'is_ok': '1',
+            'scene': '124',
+            'uin': 'MTY4MDI2MzkxNA==',
+            'key': key,
+            'pass_ticket': pass_ticket,
+            'appmsg_token': appmsg_token,
+            'x5': '0',
+            'f': 'json'
+        }
+
+
+def get_request_url():
+        return json.load(open('config/url.json', 'r', encoding='utf-8'))['url']
+
+
+def get_request_header():
+        return json.load(open('config/header.json', 'r', encoding='utf-8'))
--- spider.py
+++ spider.py
@@ -1,60 +1,58 @@
 import requests
 import time
+import os
 import json
 import pandas as pd
+import config.RequestParams as params
 
 class PassageSpider:
 
-    def __init__(self,key, biz,
+    def __init__(self,
+                 key,
+                 biz,
                  offset='0',
-                 count='10',
-                 appmsg_token=''
+                 count='10'
                  ):
-        self.pass_url = "https://mp.weixin.qq.com/mp/profile_ext"
+        self.offset = offset
+        self.count = count
+        self.sleeptime = 3 #休眠3秒
+        self.pass_url = params.get_request_url()
         self.datatmsp = pd.DataFrame(columns=['id', 'title', 'url', 'datetime', 'copyright'])
-        self.headers = {
-            'Host': 'mp.weixin.qq.com',
-            'Connection': 'keep-alive',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/'
-                      'signed-exchange;v=b3;q=0.9',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
-                          '80.0.3987.163 Safari/537.36',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-        }
-        # key,__biz和appmsg_token的属性是不一样的
-        self.params = {
-            'action': 'getmsg',
-            '__biz': biz,
-            'f': 'json',
-            'offset': offset,  # offset是改变的，它的值是上次的offset的值加上上次的count值
-            'count': count,
-            'is_ok': '1',
-            'scene': '124',
-            'uin': 'MTY4MDI2MzkxNA==',
-            'key': key,
-            'pass_ticket': 'wyeBEKFvPyKdz2dFi+yXTiGxxA0aIrFdRhi9vefIjZRfpRFArkBheJEh7X2oXe7M',
-            'appmsg_token': appmsg_token,
-            'x5': '0',
-            'f': 'json'
-        }
+        self.headers = params.get_request_header()
+        self.params = params.get_request_params(biz, offset, count, key)
 
-    def request_url(self):
+    def request_url(self, getall):
+        '''
+        getall参数表达的含义是是否获取微信公众号的所有文章，为True时表示获取全部
+        :param getall:
+        :return:
+        '''
         requests.packages.urllib3.disable_warnings()
         result = requests.get(url=self.pass_url,
                               headers=self.headers,
                               params=self.params,
                               verify=False)
         html = json.loads(result.text)
-        return html
+        self.parse_json(html, getall)
 
-    def parse_json(self,html):
+    def parse_json(self, html, getall):
+        '''
+        解析html页面
+        :param html:
+        :return:
+        '''
+        can_msg_continue = html['can_msg_continue']
+        next_offset = html['next_offset']
         json_list = json.loads(html['general_msg_list'])['list']
+
         for item in json_list:
             id = item['comm_msg_info']['id']
             datatime = item['comm_msg_info']['datetime']
             time_ = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(int(datatime)))
-            title_ = item['app_msg_ext_info']['title']
+            try:
+                title_ = item['app_msg_ext_info']['title']
+            except:
+                continue
             content_url_ = item['app_msg_ext_info']['content_url']
             try:
                 copyright_ = item['app_msg_ext_info']['copyright_stat']
@@ -63,7 +61,7 @@ class PassageSpider:
             eleinums = item['app_msg_ext_info']['multi_app_msg_item_list']
 
             if title_ != "":
-                print(id, title_, content_url_, time_)
+                # print(id, title_, content_url_, time_)
                 df_insert = pd.DataFrame({'id':[id],
                                           'title':[title_],
                                           'url':[content_url_],
@@ -80,49 +78,72 @@ class PassageSpider:
                         copyright_ = ele['copyright_stat']
                     except:
                         copyright_ = ''
-                    print(id, title_, content_url_, time_)
+                    # print(id, title_, content_url_, time_)
                     df_insert = pd.DataFrame({'id': [id],
                                               'title': [title_],
                                               'url': [content_url_],
                                               'datetime': [time_],
-                                              'copyright':[copyright_]
+                                              'copyright': [copyright_]
                                               })
                     self.datatmsp = self.datatmsp.append(df_insert, ignore_index=True)
+        print(self.datatmsp)
+        if can_msg_continue == 1 and getall:
+            time.sleep(self.sleeptime)
+            self.params = params.get_request_params(biz, next_offset, self.count, key)
+            self.request_url(getall=True)
 
-    def save_xls(self):
-        self.datatmsp.to_csv('data/datastmsp.csv',
+    def save_xls(self, getall, filename='passagedata.csv'):
+        '''
+        将数据保存到csv格式的文件中，并使用追加添加的模式
+        :return:
+        '''
+        if getall:
+            titledata = pd.DataFrame(columns=['id', 'title', 'url', 'datetime', 'copyright'])
+            titledata.to_csv(os.path.join('data', filename),
+                                encoding='utf_8_sig',
+                                index=False)
+        self.datatmsp.to_csv(os.path.join('data', filename),
                              mode='a',
                              encoding='utf_8_sig',
                              index=False,
                              header=False)
 
     def url2pdf(self):
+        '''
+        将获取到的公众号url保存为pdf单独的文件
+        :return:
+        '''
         pass
 
+
 if __name__ == "__main__":
-    offset = 0
-    #每页10个
-    count = 10
-    #爬取20页
-    pages = 2
-    #时间间隔，建议长一点
-    interval = 5
-    #每个公众号都会变的地方
-    biz = 'MjM5NDU1MDU2Nw=='
-    key = '4bc1bbadb5ce2652bdff9f69406a6806f545bc01e287d131ce9a1e5d0bbcf390234040870ee4c11467faece7e3c53765b29ef7c8b44845d20cb84cb7df8a478d532179d5d6082b9bac780f6a2bc9e4e9'
+    option = input('爬取全部输入‘all’，自定义页数输入页数，（例如：‘2’）：')
+    filename = 'datastmp.csv'
+    # 每个公众号都会变的地方
+    biz = 'MzI3ODYyNjEwNQ=='
+    key = '0bede9ac882f25ac7aa5df8e929e74045e9beb69489883fc944079865128faa0ebf4830eb6d8a2eb502146422d54ae8b99f352c4f6533c83dce443f681ab194a8bec9569a513bc1b6e3c76dbd2bd8cf5'
 
-    titledata = pd.DataFrame(columns=['id', 'title', 'url', 'datetime', 'copyright'])
-    titledata.to_csv('data/datastmsp.csv',
-                     encoding='utf_8_sig',
-                     index=False)
-
-    for i in range(pages):
-        spider = PassageSpider(offset=offset,
-                               count=count,
+    if option == 'all':
+        spider = PassageSpider(offset=0,
+                               count=10,
                                biz=biz,
                                key=key)
-        html = spider.request_url()
-        spider.parse_json(html)
-        offset += count
-        time.sleep(interval)
-        spider.save_xls()
+        spider.request_url(getall=True)
+        spider.save_xls(getall=True, filename=filename)
+    else:
+        pages = int(option)
+        offset = 0
+        count =10
+        titledata = pd.DataFrame(columns=['id', 'title', 'url', 'datetime', 'copyright'])
+        titledata.to_csv(os.path.join('data', filename),
+                         encoding='utf_8_sig',
+                         index=False)
+        for i in range(pages):
+            spider = PassageSpider(offset=offset,
+                                   count=count,
+                                   biz=biz,
+                                   key=key)
+            spider.request_url(getall=False)
+            offset += count
+            time.sleep(spider.sleeptime)
+            spider.save_xls(getall=False, filename=filename)
