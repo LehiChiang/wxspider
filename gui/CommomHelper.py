67a6ea6964c7486c2bff471657c62a03a08d10a3
Lehi Chiang <1471277588@qq.com>
2020-04-26 09:31:45 +0800

    添加工具类

---
+++ gui/CommomHelper.py
@@ -0,0 +1,11 @@
+'''
+    工具类
+'''
+class CommonHelper:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def readQss(style):
+        with open(style, 'r') as f:
+            return f.read()
